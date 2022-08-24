419405b7661a75bd581e6e674b7d84ddfa94e813
JadziaRydzyk <jadzia.szy@gmail.com>
2022-08-24 11:05:32 +0200

    added search engine

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <div clasName="container">
+      <div className="container">
         <main>
           <Dictionary />
         </main>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Dictionary.css";
 
 export default function Dictionary() {
+  const [keyword, setKeyword] = useState("");
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
   return (
     <div className="Dictionary">
-      <form>
-        <input type="search" autoFocus={true} />
-        <input type="submit" value="Search" />
+      <form
+        onSubmit={search}
+        className="SearchForm form-inline align-items-center"
+      >
+        <div className="form-group mx-sm-3 mb-2">
+          <input
+            type="search"
+            autoFocus={true}
+            className="form-control"
+            onChange={handleKeywordChange}
+          />
+        </div>
+        <input type="submit" value="Search" className="btn btn-primary mb-2" />
       </form>
     </div>
   );
