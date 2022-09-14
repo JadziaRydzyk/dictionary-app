2e185d34b8cb910bffe7cebeeeda2dc675554e82
JadziaRydzyk <jadzia.szy@gmail.com>
2022-09-14 13:20:27 +0200

    added photos API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,30 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
   const [results, setResults] = useState(null);
+  const [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
   function search(event) {
     event.preventDefault();
-
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f9170000100000157e7829701ae453cb67776f2846e8191";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -25,6 +35,8 @@ export default function Dictionary() {
       <section className="SearchForm">
         <form onSubmit={search} className=" SearchForm">
           <div className="">
+            {" "}
+            <div className="searchHint">What word do you want to look up?</div>
             <input
               type="search"
               autoFocus={true}
@@ -35,6 +47,7 @@ export default function Dictionary() {
         </form>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
